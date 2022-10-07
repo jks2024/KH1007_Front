fd2ec24ffb70d11c3ef560c06840ecd7a1884c40
jks2024 <jks2024@gmail.com>
2022-10-07 11:29:56 +0900

    수정

--- 회원가입페이지만들기/signup.js
+++ 회원가입페이지만들기/signup.js
@@ -17,31 +17,31 @@ pwBtn.addEventListener("click", function(e) {
     checkPW(pwVal);
 });
 
+let idCheck = document.querySelector(".id_input_check");
 function checkID(val) {
     const regex = /^[a-z]+[a-z0-9]{8,20}$/g;
     if(regex.test(val)) {
         isIdValid = true;
-        document.querySelector(".id_input_check").textContent = "사용가능한 아이디 입니다.";
-        document.querySelector(".id_input_check").style.color = "blue";
+        idCheck.textContent = "사용가능한 아이디 입니다.";
+        idCheck.style.color = "blue";
     } else {
         isIdValid = false;
-        document.querySelector(".id_input_check").textContent = "아이디 입력 조건이 맞지 않습니다";
-        document.querySelector(".id_input_check").style.color = "red";
+        idCheck.textContent = "아이디 입력 조건이 맞지 않습니다";
+        idCheck.style.color = "red";
     }
 }
 
+let pwCheck = document.querySelector(".pw_input_check");
 function checkPW(val) {
     console.log(val);
     const regex = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/;
     if (regex.test(val)) {
         isPwValid = true;
-        document.querySelector(".pw_input_check").textContent = "사용가능한 비밀번호 입니다.";
-        document.querySelector(".pw_input_check").style.color = "blue";
+        pwCheck.textContent = "사용가능한 비밀번호 입니다.";
+        pwCheck.style.color = "blue";
     } else {
-        // document.querySelector(".pw_input_check").textContent = "비밀번호 입력 조건이 맞지 않습니다.";
-        // document.querySelector(".pw_input_check").style.color = "red";
-        document.querySelector(".pw_input_check").textContent = "사용가능한 비밀번호 입니다.";
-        document.querySelector(".pw_input_check").style.color = "blue";
+        pwCheck.textContent = "비밀번호 입력 조건이 맞지 않습니다.";
+        pwCheck.style.color = "red";
         isPwValid = false;
     }
 }
